e9adaeb67e8f096ef95239ca4ce9d6d6e3e22657
walterarable <walter.jove@arable.com>
2019-10-29 18:07:20 -0700

    changing names

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ with open('requirements.txt') as f:
     requirements = f.read().split('\n')
 
 setup(
-   name='arable-python',
+   name='arablepy',
    version='1.0',
    description='A useful module for making calls to arable api v2',
    author='Walter Jove',
    author_email='walter.jove@arable.com',
    url="https://github.com/walterarable/arable-python-v2",
-   packages=['arable-python'],  #same as name
+   packages=['arablepy'],  #same as name
    install_requires=requirements #external packages as dependencies
 )
