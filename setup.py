185e61ff2d5ab5e38e7e26857be2c9fb16f520d7
walterarable <walter.jove@arable.com>
2019-10-30 09:41:36 -0700

    changing names

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
    description='A useful module for making calls to arable api v2',
    author='Walter Jove',
    author_email='walter.jove@arable.com',
-   url="https://github.com/walterarable/arable-python-v2",
+   url="https://github.com/walterarable/arable-python-v2.git",
    packages=['arablepy'],  #same as name
    install_requires=requirements #external packages as dependencies
 )
