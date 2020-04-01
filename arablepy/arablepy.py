4bbc0f8dd0293879eee284998076507c7adbe39f
walterarable <walter.jove@arable.com>
2020-03-31 17:57:12 -0700

    Update arablepy.py

--- arablepy/arablepy.py
+++ arablepy/arablepy.py
@@ -204,7 +204,7 @@ class ArableClient(object):
             
 
 
-    def orgs(self, org_id=None, users=False, df=False):
+    def orgs(self, org_id=None, param, df=False):
         """
         """
         self._check_connection()
@@ -212,8 +212,8 @@ class ArableClient(object):
         url = ArableClient._base_url + "/orgs"
         if org_id:
             url += '/' + org_id
-        if users:
-            url += '/' + 'users'
+        if param:
+            url += '/' + param
         return self._output(url=url, df=df, header=self.header)
 
     def search(self, name, suggest=False, df=False):
